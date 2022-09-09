81f93cf37bf61cbc32c5a1d1298c51f425576a00
Aaron Cope <apc42190@csu.fullerton.edu>
2022-09-08 18:02:10 -0700

    Added assignment 1

---
+++ Assignment_1/least_squared_mean.py
@@ -0,0 +1,24 @@
+import seaborn as sns
+import numpy as np
+import pandas as pd
+
+
+df = sns.load_dataset('iris')
+print(df.head())
+
+dependent_column = 'sepal_length'
+features = ['sepal_width', 'petal_length', 'petal_width']
+
+coefficients = []
+
+for order in range(1, 10):
+    modified_df = df
+    betas = []
+    for feature in features:
+        column = df[[feature]]**order
+        beta = np.linalg.inv(column.T.dot(column)).dot(column.T).dot(df[dependent_column].fillna(0))    
+        betas.append(beta[0])
+    coefficients.append(betas)
+
+for function in coefficients:
+    print(function)
