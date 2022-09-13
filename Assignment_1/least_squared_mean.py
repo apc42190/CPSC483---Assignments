965d9aa1e1b3d0f0146b481454f1696164f4dc97
Aaron Cope <apc42190@csu.fullerton.edu>
2022-09-12 19:35:39 -0700

    Formatting output

--- Assignment_1/least_squared_mean.py
+++ Assignment_1/least_squared_mean.py
@@ -14,14 +14,21 @@ y = df['Idx']
 X_training_data, X_testing_data, y_training_data, y_testing_data = model_selection.train_test_split(X, y, test_size = 0.2, random_state = 1)
 
 for order in range(1, 10):
-    polinomial_features = PolynomialFeatures(degree = order)
+    polynomial_features = PolynomialFeatures(degree = order)
 
     current_time = time.time()
-    X_poly_training_data = polinomial_features.fit_transform(X_training_data)
+    X_poly_training_data = polynomial_features.fit_transform(X_training_data)
     model = linear_model.LinearRegression()
     model.fit(X_poly_training_data, y_training_data)
-    training_time = time.time() - current_time
+    training_time = "{:.5f}".format(time.time() - current_time)
 
     y_hat = model.predict(X_poly_training_data)
+    training_RMSE = "{:.5f}".format(mean_squared_error(y_training_data, y_hat, squared = False))
+    training_R_2 = "{:.5f}".format(r2_score(y_training_data, y_hat))
+    
+    X_poly_testing_data = polynomial_features.fit_transform(X_testing_data)
+    y_hat = model.predict(X_poly_testing_data)
+    testing_RMSE = "{:.5f}".format(mean_squared_error(y_testing_data, y_hat, squared = False)) 
+    testing_R_2 = "{:.5f}".format(r2_score(y_testing_data, y_hat))
 
-    print(f'Order = {order}, RMSE = {mean_squared_error(y_training_data, y_hat, squared = False)}, R^2 = {r2_score(y_training_data, y_hat)}, training_time = {training_time} seconds')
+    print(f'Order: {order}, Training Time: {training_time} secs, Training RMSE: {training_RMSE}, Traingin R^2: {training_R_2}, Testing RMSE: {testing_RMSE}, Testing R^2: {testing_R_2}')
