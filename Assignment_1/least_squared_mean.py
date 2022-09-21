bd1fd3763086304339e7244c4350b83d91ea590d
Aaron Cope <apc42190@csu.fullerton.edu>
2022-09-20 17:30:38 -0700

    Simplified output

--- Assignment_1/least_squared_mean.py
+++ Assignment_1/least_squared_mean.py
@@ -16,18 +16,18 @@ y = df['Idx']
 X_training_data, X_testing_data, y_training_data, y_testing_data = model_selection.train_test_split(X, y, test_size = 0.4)
 
 for order in range(1, 10):
-    # polynomial_features = PolynomialFeatures(degree = order)
+    polynomial_features = PolynomialFeatures(degree = order)
 
-    # X_poly_training_data = polynomial_features.fit_transform(X_training_data)
-    # X_poly_testing_data = polynomial_features.fit_transform(X_testing_data)
+    X_poly_training_data = polynomial_features.fit_transform(X_training_data)
+    X_poly_testing_data = polynomial_features.fit_transform(X_testing_data)
     
-    X_poly_training_data =  X_training_data.copy()
-    X_poly_testing_data = X_testing_data.copy()
+    # X_poly_training_data =  X_training_data.copy()
+    # X_poly_testing_data = X_testing_data.copy()
     
-    for power in range(2, order + 1):
-        for feature in features:
-            X_poly_training_data[f'{feature}^{power}'] = X_poly_training_data[feature]**power
-            X_poly_testing_data[f'{feature}^{power}'] = X_poly_testing_data[feature]**power
+    # for power in range(2, order + 1):
+    #     for feature in features:
+    #         X_poly_training_data[f'{feature}^{power}'] = X_poly_training_data[feature]**power
+    #         X_poly_testing_data[f'{feature}^{power}'] = X_poly_testing_data[feature]**power
 
 
     current_time = time.time()
@@ -38,13 +38,13 @@ for order in range(1, 10):
     training_time = "{:.2f}".format(time.time() - current_time)
     
     
-    y_hat = model.predict(X_poly_training_data)
-    training_RMSE = "{:.5f}".format(mean_squared_error(y_training_data, y_hat, squared = False)) 
-    training_R_2 = "{:.5f}".format(r2_score(y_training_data, y_hat))
+    # y_hat = model.predict(X_poly_training_data)
+    # training_RMSE = "{:.5f}".format(mean_squared_error(y_training_data, y_hat, squared = False)) 
+    # training_R_2 = "{:.5f}".format(r2_score(y_training_data, y_hat))
 
     y_hat = model.predict(X_poly_testing_data)
     testing_RMSE = "{:.5f}".format(mean_squared_error(y_testing_data, y_hat, squared = False)) 
     testing_R_2 = "{:.5f}".format(r2_score(y_testing_data, y_hat))
 
-    print(f'Order: {order}, Training Time: {training_time} secs, Training RMSE: {training_RMSE}, Testing RMSE: {testing_RMSE}, Training R^2: {training_R_2}, Testing R^2: {testing_R_2}')
+    print(f'Order: {order}, Training Time: {training_time} secs, RMSE: {testing_RMSE}, R^2: {testing_R_2}')
     #print(f'Intercept: {model.intercept_}, Weights: {model.coef_}\n')
