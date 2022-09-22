54e1c19a12b73061ed99b8447cca46452e5c5587
tish978 <satish.bisa@gmail.com>
2022-09-22 13:33:32 -0700

    Scaling calculations.

---
+++ Assignment_1/feature-scaling-problem.py
@@ -0,0 +1,72 @@
+import numpy as np
+import pandas as pd
+from sklearn import model_selection
+from sklearn import linear_model
+from sklearn import preprocessing
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.metrics import r2_score, mean_squared_error
+import time
+
+#REFERENCED LINK: https://www.geeksforgeeks.org/ml-feature-scaling-part-2/
+
+
+#Data to be read
+#dataSet = pd.read_csv('test-scaled-data.csv')
+dataSet = pd.read_csv('Data1.csv')
+
+X = dataSet.iloc[:, 0:4].values
+y = dataSet.iloc[:, 4:5].values
+
+#Divide dataframe into depenant and independent variables
+features = ['T', 'P', 'TC', 'SV']
+X_str = dataSet[features]
+y_str = dataSet['Idx']
+
+
+print("\nOriginal data values : \n", X)
+print("\nOriginal Idx values : \n", y)
+
+# MIN MAX SCALER
+min_max_scaler = preprocessing.MinMaxScaler(feature_range=(0, 1))
+y_min_max_scaler = preprocessing.MinMaxScaler(feature_range=(5,5))
+
+# Scaled feature
+x_after_min_max_scaler = min_max_scaler.fit_transform(X)
+y_after_min_max_scaler = min_max_scaler.fit_transform(y)
+
+#x_after_min_max_scaler = min_max_scaler
+#y_after_min_max_scaler = min_max_scaler
+
+print("\nAfter min max scaling : \n", x_after_min_max_scaler)
+print("\ny After min max scaling : \n", y_after_min_max_scaler)
+
+# Split data between training and testing
+X_training_data, X_testing_data, y_training_data, y_testing_data = model_selection.train_test_split(X_str, y_str, test_size = 0.4)
+
+for order in range(1, 11):
+
+    current_time = time.time()
+
+    # Generate Higher Order Terms: No Interations
+    X_poly_training_data = X_training_data.copy()
+    X_poly_testing_data = X_testing_data.copy()
+    for power in range(2, order + 1):
+        for feature in features:
+            X_poly_training_data[f'{feature}^{power}'] = X_poly_training_data[feature] ** power
+            X_poly_testing_data[f'{feature}^{power}'] = X_poly_testing_data[feature] ** power
+
+    # Train model
+    model = linear_model.LinearRegression()
+    model.fit(X_poly_training_data, y_training_data)
+
+    training_time = "{:.2f}".format(time.time() - current_time)
+
+    # Test model with testing data
+    y_hat = model.predict(X_poly_testing_data)
+
+    testing_RMSE = "{:.5f}".format(mean_squared_error(y_testing_data, y_hat, squared=False))
+    testing_R_2 = "{:.5f}".format(r2_score(y_testing_data, y_hat))
+
+    print(f'Order: {order}, Training Time: {training_time} secs, RMSE: {testing_RMSE}, R^2: {testing_R_2}')
+    #print(f'Intercept: {model.intercept_}, Weights: {model.coef_}\n')
+
