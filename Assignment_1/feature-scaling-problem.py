fdc928d800d65cea2681dcda62e561175662d8c5
Aaron Cope <apc42190@csu.fullerton.edu>
2022-09-22 14:46:55 -0700

    Added training error to output

--- Assignment_1/feature-scaling-problem.py
+++ Assignment_1/feature-scaling-problem.py
@@ -30,10 +30,6 @@ min_max_scaler = preprocessing.MinMaxScaler(feature_range=(0, 1))
 # Scaled feature
 X[features] = min_max_scaler.fit_transform(X)
 
-#x_after_min_max_scaler = min_max_scaler
-
-print("\nAfter min max scaling : \n", X)
-
 # Split data between training and testing
 X_training_data, X_testing_data, y_training_data, y_testing_data = model_selection.train_test_split(X, y, test_size = 0.5)
 
@@ -55,12 +51,15 @@ for order in range(1, 11):
 
     training_time = "{:.2f}".format(time.time() - current_time)
 
-    # Test model with testing data
+  #Test model with training data
+    y_hat = model.predict(X_poly_training_data)
+    training_RMSE = "{:.5f}".format(mean_squared_error(y_training_data, y_hat, squared = False)) 
+    training_R_2 = "{:.5f}".format(r2_score(y_training_data, y_hat))
+    
+    #Test model with testing data
     y_hat = model.predict(X_poly_testing_data)
-
-    testing_RMSE = "{:.5f}".format(mean_squared_error(y_testing_data, y_hat, squared=False))
+    testing_RMSE = "{:.5f}".format(mean_squared_error(y_testing_data, y_hat, squared = False)) 
     testing_R_2 = "{:.5f}".format(r2_score(y_testing_data, y_hat))
 
-    print(f'Order: {order}, Training Time: {training_time} secs, RMSE: {testing_RMSE}, R^2: {testing_R_2}')
+    print(f'Order: {order}, Training Time: {training_time} secs, Testing RMSE: {testing_RMSE}, Training RMSE: {training_RMSE}, Testing R^2: {testing_R_2}, Training R^2: {training_R_2}')
     #print(f'Intercept: {model.intercept_}, Weights: {model.coef_}\n')
-
