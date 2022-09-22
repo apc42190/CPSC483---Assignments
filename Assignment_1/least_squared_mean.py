8ac081eb6f24cdcf6e9dcedcb91fc240e2f298aa
Aaron <apc42190@yahoo.com>
2022-09-22 12:28:37 -0700

    Whatever

--- Assignment_1/least_squared_mean.py
+++ Assignment_1/least_squared_mean.py
@@ -15,7 +15,7 @@ X = df[features]
 y = df['Idx']
 
 #Split data between training and testing
-X_training_data, X_testing_data, y_training_data, y_testing_data = model_selection.train_test_split(X, y, test_size = 0.4)
+X_training_data, X_testing_data, y_training_data, y_testing_data = model_selection.train_test_split(X, y, test_size = 0.5)
 
 for order in range(1, 11):
     
@@ -42,10 +42,15 @@ for order in range(1, 11):
     training_time = "{:.2f}".format(time.time() - current_time)
 
 
+    #Test model with training data
+    y_hat = model.predict(X_poly_training_data)
+    training_RMSE = "{:.5f}".format(mean_squared_error(y_training_data, y_hat, squared = False)) 
+    training_R_2 = "{:.5f}".format(r2_score(y_training_data, y_hat))
+    
     #Test model with testing data
     y_hat = model.predict(X_poly_testing_data)
     testing_RMSE = "{:.5f}".format(mean_squared_error(y_testing_data, y_hat, squared = False)) 
     testing_R_2 = "{:.5f}".format(r2_score(y_testing_data, y_hat))
 
-    print(f'Order: {order}, Training Time: {training_time} secs, RMSE: {testing_RMSE}, R^2: {testing_R_2}')
+    print(f'Order: {order}, Training Time: {training_time} secs, Testing RMSE: {testing_RMSE}, Training RMSE: {training_RMSE}, Testing R^2: {testing_R_2}, Training R^2: {training_R_2}, Terms: {len(X_poly_training_data.columns)}')
     #print(f'Intercept: {model.intercept_}, Weights: {model.coef_}\n')
