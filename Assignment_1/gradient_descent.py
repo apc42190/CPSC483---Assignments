77069e68d9ded1dc40a92831a24dcf9ab896f2e4
Christopher Phongsa <cphongsa@csu.fullerton.edu>
2022-09-26 23:46:24 -0700

    Add higher order calculations

--- Assignment_1/gradient_descent.py
+++ Assignment_1/gradient_descent.py
@@ -3,35 +3,35 @@ import pandas as pd
 from sklearn import model_selection
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.metrics import r2_score, mean_squared_error
+import matplotlib.pyplot as plt
 import time
 
-def gradient_descent(X,y,iterations,learning_rate):
-    w0 = w1 = w2 = w3 = w4 = 0
+def gradient_descent(X,y,features,iterations,learning_rate):
     n = len(X)
+    features_length = len(features)
+    weights = {}
 
+    for i in range(features_length):
+        weights[f'w{i}'] = 0
+    
     for i in range(iterations):
-        w = np.array([w1,w2,w3,w4])
-        y_hat = X.dot(w.T)
-        loss = (1/(2*n))*np.sum(np.square(y_hat-y))
+        w = np.array(list(weights.values()), dtype=np.float128)
+
+        y_hat = np.dot(X, w.T)
+
+        error = y - y_hat
+
+        loss = np.float128(np.sqrt(((1/(2*n))*np.sum(np.square(error)))))
 
         if np.all(np.abs(loss) <= 1e-06):
             break
 
-        w0d = learning_rate*(1/n)*np.sum((y_hat - y))
-        w1d = learning_rate*(1/n)*np.sum(X.iloc[:, 0] * (y_hat - y))
-        w2d = learning_rate*(1/n)*np.sum(X.iloc[:, 1] * (y_hat - y))
-        w3d = learning_rate*(1/n)*np.sum(X.iloc[:, 2] * (y_hat - y))
-        w4d = learning_rate*(1/n)*np.sum(X.iloc[:, 3] * (y_hat - y))
-        
-        w0 = w0 - w0d
-        w1 = w1 - w1d
-        w2 = w2 - w2d
-        w3 = w3 - w3d
-        w4 = w4 - w4d
+        for j in range(features_length):
+            w_deriv = -learning_rate*((1/n)*(np.sum(np.dot(X.iloc[:,j],error))))            
+            weights[f'w{j}'] = float(weights[f'w{j}'] - w_deriv)
 
-        # TODO: create a tolerance to skip loop
+        print("loss/cost {}, iteration {}".format(loss,i))
 
-        print("w0 {}, w1 {}, w2 {}, w3 {}, w4 {}, loss/cost {}, iteration {}".format(w0,w1,w2,w3,w4,loss,i))
 
 df = pd.read_csv('Data1.csv')
 
@@ -40,6 +40,18 @@ X = df[features]
 y = df['Idx']
 
 # split the data between training and testing
-X_training_data, X_testing_data, y_training_data, y_testing_data = model_selection.train_test_split(X, y, test_size = 0.4)
+X_training_data, X_testing_data, y_training_data, y_testing_data = model_selection.train_test_split(X, y, test_size = 0.5)
 
-gradient_descent(X_training_data,y_training_data,1000,0.00001)
+for order in range(1,2):
+    X_poly_training_data = X_training_data.copy()
+    X_poly_testing_data = X_testing_data.copy()
+
+    for power in range(2, order + 1):
+        for feature in features:
+            X_poly_training_data[f'{feature}^{power}'] = X_poly_training_data[feature]**power
+            X_poly_testing_data[f'{feature}^{power}'] = X_poly_testing_data[feature]**power
+
+X_poly_training_data.insert(0, 'intercept', 1)
+X_poly_features = list(X_poly_training_data.columns)
+
+gradient_descent(X_poly_training_data,y_training_data,X_poly_features,2000,0.000007)
