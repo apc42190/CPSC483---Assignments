b98231a4ad9674db906338087e5996298dfaffda
Christopher Phongsa <cphongsa@csu.fullerton.edu>
2022-09-28 19:29:27 -0700

    Normalize data

--- Assignment_1/gradient_descent.py
+++ Assignment_1/gradient_descent.py
@@ -1,9 +1,9 @@
 import numpy as np
 import pandas as pd
 from sklearn import model_selection
+from sklearn import preprocessing
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.metrics import r2_score, mean_squared_error
-import matplotlib.pyplot as plt
 import time
 
 def gradient_descent(X,y,features,iterations,learning_rate):
@@ -32,6 +32,8 @@ def gradient_descent(X,y,features,iterations,learning_rate):
 
         print("loss/cost {}, iteration {}".format(loss,i))
 
+    print('weights \n{}'.format(weights))
+
 
 df = pd.read_csv('Data1.csv')
 
@@ -39,10 +41,15 @@ features = ['T', 'P', 'TC', 'SV']
 X = df[features]
 y = df['Idx']
 
+# MIN MAX SCALER
+min_max_scaler = preprocessing.MinMaxScaler(feature_range=(0,1))
+
+X[features] = min_max_scaler.fit_transform(X)
+
 # split the data between training and testing
 X_training_data, X_testing_data, y_training_data, y_testing_data = model_selection.train_test_split(X, y, test_size = 0.5)
 
-for order in range(1,2):
+for order in range(1,11):
     X_poly_training_data = X_training_data.copy()
     X_poly_testing_data = X_testing_data.copy()
 
@@ -54,4 +61,4 @@ for order in range(1,2):
 X_poly_training_data.insert(0, 'intercept', 1)
 X_poly_features = list(X_poly_training_data.columns)
 
-gradient_descent(X_poly_training_data,y_training_data,X_poly_features,2000,0.000007)
+gradient_descent(X_poly_training_data,y_training_data,X_poly_features,1000,0.2)
